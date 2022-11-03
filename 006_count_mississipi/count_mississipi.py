1d67e39e55e9f035b7da9885b6ed4d345a385823
C.J. Costa <ccosta@estgl.ipv.pt>
2022-11-03 13:26:34 +0000

    Implementation of simulate count mississipi game

---
+++ 006_count_mississipi/count_mississipi.py
@@ -0,0 +1,11 @@
+# count_mississipi.py
+# simulate count mississipily an US count on play hide and seek
+
+import time
+
+for i in range(1,6): # Write a for loop that counts to five.
+    print(i, 'Mississippi')   # Body of the loop - print the loop iteration number and the word "Mississippi".
+    time.sleep(1)               # Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
+print("Ready or not, here I come!")
